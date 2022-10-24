17096215627a5fbea14a72ac56a2dcd1ba0eb5a6
miguelmnts <miguel.mnts.c@gmail.com>
2022-10-24 16:28:27 +0200

    Ejercicio1Bloque1

--- git_leaks.py
+++ git_leaks.py
@@ -7,72 +7,83 @@ import re
 import signal
 import sys
 import time
-import pwn
+#import pwn
 import pdb
 
 
 
 def handler_signal(signal, frame):
-    print('\n\n [!] Out .............. \n')
-    sys.exit(1)
+	print('\n\n [!] Out .............. \n')
+	sys.exit(1)
 
 signal.signal(signal.SIGINT, handler_signal)
 
-
-REPO_DIR = './skale/skale-manager'
 
 
 # time.sleep(15)
 
 def extract(path):
-    #Repo permite especificar la url de un repositorio que luego se guarda en la variable especificada
-    repo = Repo(path)
-    # print(repo)
-    # print(repo.status)
+	#Repo permite especificar la url de un repositorio que luego se guarda en la variable especificada
+	repo = Repo(path)
+	# print(repo)
+	# print(repo.status)
 
-    print('Repository extracted')
-    time.sleep(1)
-    return repo.iter_commits()
+	print('Repository extracted')
+	time.sleep(1)
+	return repo.iter_commits()
+
+
 
 def transform(commits):
 
-    commits = list(commits)
-    Reference = len(commits)
+	#list with all commit objects
+	commits = list(commits)
 
-    processedCommits = list()
-    iterationNumber = 0
-    for i in list(commits):
-        iterationNumber += 1
-        #if 'a' in str(i.message):
-        patterns = ['password','key','security','critical']
-        for pattern in patterns:
-            if ( bool(re.search(pattern, i.message)) ):
-                processedCommits.append(i)
-        print('Filtering commits:')
-        print(str(int((iterationNumber/Reference)*100))+'%','[' + ''.join(['=' for i in range(int((iterationNumber/Reference)*100))])+ str('>') + ''.join([' ' for i in '=' for i in range(int((1 - (iterationNumber/Reference))*100))]) +']')
-        time.sleep(0.001)
-        os.system('cls')
 
-    return processedCommits
+	#variables for the progress bar
+	Reference = len(commits) 
+	iterationNumber = 0
+	progress = 0
+
+	#list where I will store the matches
+	processedCommits = list()
+
+	for i in commits:
+		iterationNumber += 1
+		patterns = [re.compile(r'password'),re.compile(r'key'),re.compile(r'security'),re.compile(r'critical')]
+		for pattern in patterns:
+			if ( bool(re.search(pattern, i.message)) ):
+				processedCommits.append(i)
+		if (iterationNumber/Reference)*100 - progress > 0:
+			progress += 1
+			print('Filtering commits:')
+			print(str(int((iterationNumber/Reference)*100))+'%','[' + ''.join(['=' for i in range(int((iterationNumber/Reference)*100))])+ str('>') + ''.join([' ' for i in '=' for i in range(int((1 - (iterationNumber/Reference))*100))]) +']')
+			time.sleep(0.001)
+			os.system('cls')
+
+	return processedCommits
+
+
 
 def load(processedCommits):
-    Register = open('commitRegister.txt', 'a')
-    print('MATCHES FOUND: ')
-    for i,commit in enumerate(processedCommits):
-        print('matched commit {j:.0f}:\n'.format(j=i) , commit.message)
-        Register.write(str(commit.message))
-        Register.write('\n\n\n')
-    Register.close()
+	Register = open('commitRegister.txt', 'w')
+	print('MATCHES FOUND: ')
+	for i,commit in enumerate(processedCommits):
+		print('matched commit {j:.0f}:\n'.format(j=i) , commit.message)
+		Register.write(str(commit.message))
+		Register.write('\n\n\n')
+	Register.close()
 
-    time.sleep(1)
+	time.sleep(1)
 
 
-#para luego poder hacer tests hay que poner un punto de entrada
+
 if __name__ == '__main__':
-    repoCommits = extract(REPO_DIR)
-    processedCommits = transform(repoCommits)
-    load(processedCommits)
-    print()
+	REPO_DIR = './skale/skale-manager'
+	repoCommits = extract(REPO_DIR)
+	processedCommits = transform(repoCommits)
+	load(processedCommits)
+	print()
 
 
 
